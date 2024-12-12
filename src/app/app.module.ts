62f4460eb8e27f4b8dac2acc07eab694bbe258d9
alejandro-source <alejnadro36@gmail.com>
2024-12-12 17:28:59 +0100

    Mi primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DbzModule } from './dbz/dbz.module';
+
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DbzModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
