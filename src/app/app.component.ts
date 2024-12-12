109b29217fa97f0912bfb247d073716bf9a3c08a
alejandro-source <alejnadro36@gmail.com>
2024-12-12 17:09:03 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-bases';
+  public title: string = 'Hola Mundo';
 }
