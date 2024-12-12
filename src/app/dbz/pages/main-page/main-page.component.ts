76abd609f9b04390b7b8b0234a4cef530ca109d0
alejandro-source <alejnadro36@gmail.com>
2024-12-12 17:16:43 +0100

    Update main-page.component.ts

--- src/app/dbz/pages/main-page/main-page.component.ts
+++ src/app/dbz/pages/main-page/main-page.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Character } from './../interfaces/character.interface';
+import { Component} from '@angular/core';
+
 
 @Component({
-  selector: 'app-main-page',
-  standalone: false,
-  templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
 })
+
 export class MainPageComponent {
-  public characters: Character[] = [
-    { name: 'Krillin', power: 1000 },
-    { name: 'Goku', power: 5000 },
-    { name: 'Vegeta', power: 4000 }
-  ];
+
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },{
+    name:'Goku',
+    power: 9500
+  },{
+    name:"Vegeta",
+    power: 7500
+  }];
+
+ // onDelete = Index value: number;
+
+  onNewCharacter(character:Character): void{
+    this.characters.push(character);
+  }
+
+  onDeleteCharacter(index:number){
+
+    //this.characters.splice(1,2)
+    this.characters.splice(index,1);
+  }
+
 }
-asdasd
