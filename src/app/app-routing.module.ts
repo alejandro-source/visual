cb30c16243e7cc2e698599c099cc401bc85597a1
alejandro-source <alejnadro36@gmail.com>
2024-12-12 20:27:36 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,3 +8,5 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//Este módulo gestiona las rutas de la aplicación, aunque en el código proporcionado no hay rutas definidas. Se configura el enrutador para que pueda ser usado en el futuro
